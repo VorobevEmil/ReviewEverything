970ff80eabcc43c2fa732a4369e6bcd90594a6dd
Эмиль Воробьёв <vorobevemil@gmail.com>
2022-12-23 23:06:09 +0300

    fix namespace in CloudImageController

--- ReviewEverything/Server/Controllers/CloudImageController.cs
+++ ReviewEverything/Server/Controllers/CloudImageController.cs
@@ -1,16 +1,6 @@
-using CloudinaryDotNet;
-using CloudinaryDotNet.Actions;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
-using PSC.Blazor.Components.MarkdownEditor.Models;
 using ReviewEverything.Server.Services.CloudImageService;
-using ReviewEverything.Shared.Contracts.Requests;
 using ReviewEverything.Shared.Models;
-using System.Net;
-using Microsoft.Net.Http.Headers;
-using ReviewEverything.Server.Common.Extensions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ReviewEverything.Server.Controllers
 {
